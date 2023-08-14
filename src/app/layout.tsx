28d4167e3465e80a5f7704cb6363b135a748bb84
Joker3052 <phuonghoangthanh3052@gmail.com>
2023-08-15 00:51:35 +0700

    jjj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+"use client"
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from 'react-bootstrap';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       
+        {children}
+       
+      </body>
     </html>
   )
 }
