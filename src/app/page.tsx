ee444f31c7f5a8bd0695516d81d82ae38387592d
Joker3052 <phuonghoangthanh3052@gmail.com>
2023-08-15 02:12:24 +0700

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,42 @@
-'use client'
 import Link from "next/link";
-import { Border } from "react-bootstrap-icons";
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+// import { Border } from "react-bootstrap-icons";
 
 export default function Home() {
   return (
     <>
-    <div className="my-4 m-[0px] px-[0px] align-center container">
-      <img className="w-full h-auto" src="/images/img_1.png" alt="" />
-    </div>
-    <div className="container">
-    <div className="m-4">
-    <h2 className="text-center font-bold leading-[48px]">Discover NEW Arrivals</h2>
-    <p className="text-center font-normal">Recently added shirts!</p>
-    </div>
-    <div className="mx-5">
-    <Row xs={1} md={4} className="g-4 mt-4">
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png"/>
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Row>
+      <div className="my-4 m-0 px-0 flex items-center justify-center">
+        <img className="w-full h-auto" src="/images/img_1.png" alt="" />
+      </div>
+      <div className="container mx-auto">
+        <div className="m-4">
+          <h2 className="text-center font-bold text-3xl leading-12">Discover NEW Arrivals</h2>
+          <p className="text-center font-normal">Recently added shirts!</p>
+        </div>
+        <div className="px-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-4">
+            <div className="border p-4">
+              <img className="w-full h-auto" src="/images/card1.png" alt="Plain White Shirt" />
+              <div className="text-center">
+                <h3 className="text-xl font-semibold">Plain White Shirt</h3>
+                <p className="text-blue-500">$123</p>
+              </div>
+            </div>
 
-    <Row xs={1} md={4} className="g-4">
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{border:0}}>
-      <Card.Img variant="top" src="/images/card1.png" />
-      <Card.Body>
-        <Card.Title className="text-center">Plain White Shirt</Card.Title>
-        <Card.Text style={{color:'blue'}} className="text-center">
-          $123
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Row>
-    </div>
-    </div>
+            {/* Lặp lại các component cho sản phẩm khác */}
+            <div className="border p-4">
+              <img className="w-full h-auto" src="/images/card2.png" alt="Another Shirt" />
+              <div className="text-center">
+                <h3 className="text-xl font-semibold">Another Shirt</h3>
+                <p className="text-blue-500">$99</p>
+              </div>
+            </div>
+
+            {/* Lặp lại các component cho sản phẩm khác */}
+          </div>
+
+          {/* Lặp lại các dòng cho các dãy sản phẩm khác */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
